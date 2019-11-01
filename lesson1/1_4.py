61ad4a4ee477328244e70cf554761c317d9100ea
Sirotenko.EA <sirotenko.ea@esphere.ru>
2019-11-01 16:46:37 +0300

    Done 1-12 from 12 tasks

---
+++ lesson1/1_4.py
@@ -0,0 +1,136 @@
+# Ветвление
+# Задание 1
+    #num = int(input())
+    #if num%2 == 0 :
+    #    print('чёт')
+    #else :
+    #    print('нечёт')
+
+# Задание 2
+    #x = int(input())
+    #y = int(input())
+    #if x > y :
+    #    print(y)
+    #else:
+    #    print(x)
+
+# Задание 3
+    #num = int(input())
+    #if num == 0:
+    #    print(0)
+    #elif num > 0:
+    #    print('1')
+    #else :
+    #    print('-1')
+
+# Задание 4
+    #num = input()
+    #if len(num) == 3 :
+    #    print('Да')
+    #else :
+    #    print('Нет')
+
+# Задание 5
+    #a = int(input())
+    #b = int(input())
+    #if a < 0 and b < 0 :
+    #    print('Нет')
+    #else :
+    #    print('Да')
+
+# Задание 6
+    #x = input()
+    #if int(x[0]) < int(x[1]) and int(x[1]) < int(x[2]) :
+    #    print('Да')
+    #else :
+    #    print('Нет')
+
+# Задание 7
+    #s = input()
+    #if int(s) == int(s[::-1]) :
+    #    print('Yes')
+    #else : 
+    #    print('No')
+
+# Задание 8
+    #num = int(input())
+    #if  num < 8 :
+    #    if num == 2 :
+    #        print(28)
+    #    elif num % 2 == 0 :
+    #        print(30)
+    #    else :
+    #        print(31)
+    #else :
+    #    if num % 2 == 0 :
+    #        print(31)
+    #    else :
+    #        print(30)
+
+# Задание 9
+    #a = int(input())
+    #b = int(input())
+    #c = int(input())
+    #
+    #if a == b == c :
+    #    print(3)
+    #elif a == b or b == c or a == c :
+    #    print(2)
+    #else :
+    #    print(0)
+
+# Задание 10
+    #y = int(input())
+    #x = int(input())
+    #if (x%2 == 0 and y%2 == 0) or (x%2 == 1 and y%2 == 1) :
+    #    print('Черная')
+    #else :
+    #    print('Белая')
+
+# Задание 11
+    #y1 = int(input())
+    #x1 = int(input())
+    #y2 = int(input())
+    #x2 = int(input())
+    #if (x1%2 == 0 and y1%2 == 0) or (x1%2 == 1 and y1%2 == 1) :
+    #    if (x2%2 == 0 and y2%2 == 0) or (x2%2 == 1 and y2%2 == 1) :
+    #        print('Да')
+    #    else :
+    #        print('Нет')
+    #else :
+    #    if (x2%2 == 0 and y2%2 == 0) or (x2%2 == 1 and y2%2 == 1) :
+    #        print('Нет')
+    #    else :
+    #        print('да')
+
+# Задание 12
+    #num = int(input())
+    #b = int(input())
+    #if  num < 8 :
+    #    if num == 2 :
+    #        if b + 1 > 28 :
+    #            print(f'1-{num+1}-2019')
+    #        else :
+    #            print(f'{b+1}-{num}-2019')
+    #    elif num % 2 == 0 :
+    #        if b + 1 > 30 :
+    #            print(f'1-{num+1}-2019')
+    #        else :
+    #            print(f'{b+1}-{num}-2019')
+    #    else :
+    #        if b + 1 > 31 :
+    #            print(f'1-{num+1}-2019')
+    #        else :
+    #            print(f'{b+1}-{num}-2019')
+    #else :
+    #    if num % 2 == 0 :
+    #        if b + 1 > 31 :
+    #            print(f'1-{num+1}-2019')
+    #        else :
+    #            print(f'{b+1}-{num}-2019')
+    #    else :
+    #        if b + 1 > 30 :
+    #            print(f'1-{num+1}-2019')
+    #        else :
+    #            print(f'{b+1}-{num}-2019')
+
