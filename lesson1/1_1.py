b7e4887c1b4c3aa0b0b05c7d7864c79564a80643
Sirotenko.EA <sirotenko.ea@esphere.ru>
2019-11-01 11:40:04 +0300

    done 7 tasks

--- lesson1/1_1.py
+++ lesson1/1_1.py
@@ -1,1 +1,45 @@
+# Ввод/Вывод
 
+# Задание 1
+    #a =int(input())
+    #b =int(input())
+    #c =int(input())
+    #print(a+b+c)
+
+# Задание 2
+    #a =int(input())
+    #b =int(input())
+    #print(a*b/2)
+
+# Задание 3
+    #a = input()
+    #print('Привет,', a + '!')
+
+# Задание 4
+    #a = input()
+    #print('Следующее число для числа', a + ':', int(a) + 1)
+    #print('Предыдущее число для числа', a + ':', int(a) - 1)
+
+# Задание 5
+    #N = int(input())
+    #K = int(input())
+    #X = f'Яблок у студентов: {K//N}, в корзине: {K%N}'
+    #print(X)
+
+# Задание 6
+    #S = int(input())
+    #print(S // 60, S // 3600)
+
+# Задание 7
+    #h1 = int(input())
+    #m1 = int(input())
+    #s1 = int(input())
+    #h2 = int(input())
+    #m2 = int(input())
+    #s2 = int(input())
+    #t1 = h1 * 3600 + m1 * 60 + s1
+    #t2 = h2 * 3600 + m2 * 60 + s2
+    #if t1 > t2:
+    #    print(t1 - t2)
+    #else :
+    #    print(t2 - t1)
