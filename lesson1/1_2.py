ec3a1db208e7589afd6e36e45a008388bbcdb459
Sirotenko.EA <sirotenko.ea@esphere.ru>
2019-11-01 13:53:03 +0300

    done tasks 2

--- lesson1/1_2.py
+++ lesson1/1_2.py
@@ -1,1 +1,47 @@
+# Числа
 
+# Задание 1
+    #x = int(input())
+    #print(x%10, x//10)
+
+# Задание 2
+    #x = int(input())
+    #print(x%10, x//10, sep='')
+
+# Задание 3
+    #x = int(input())
+    #if x%100 == 0 :
+    #    print('00')
+    #else : 
+    #    print(x%100)
+
+# Задание 4
+    #x = int(input())
+    #print((x%100)//10)
+
+# Задание 5
+    #x = int(input())
+    #print(x//100 + (x%100)//10 + x%10)
+
+# Задание 6
+    #x = float(input())
+    #print(int(x*10%10))
+
+# Задание 7
+    #x = int(input())
+    #print(x // 100 + int(bool(x % 100)))
+
+# Задание 8
+    #N = int(input()) + 3
+    #print(N % 7)
+
+# Задание 9 - Не хватает 0
+    #N = int(input())
+    #print(N//60, N%60, sep= ':')
+
+# Задание 10
+    #from math import ceil
+    #a = int(input())
+    #b = int(input())
+    #c = int(input())
+    #print(ceil(a/2) + ceil(b/2) + ceil(c/2))
