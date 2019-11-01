dbdd6dd1a97e99a31eb8baf6fdd6529d3d53b7e6
Sirotenko.EA <sirotenko.ea@esphere.ru>
2019-11-01 18:01:41 +0300

    Done 1-5 from 12 tasks

---
+++ lesson1/1_5.py
@@ -0,0 +1,50 @@
+# Цикл While
+
+# Задание 1
+    #x = int(input())
+    #i = 2
+    #while x % i != 0:
+    #    i+=1
+    #print(i)
+
+# Задание 2
+    #x = int(input())
+    #n = 0
+    #while 2 ** n < x:
+    #    if 2 ** (n +1) > x :
+    #        print(n, 2**n)
+    #        break
+    #    n += 1
+
+# Задание 3
+    #X = int(input())
+    #Y = int(input())
+    #i = 1
+    #while X < Y:
+    #   X += X * 0.1
+    #   i+=1 
+    #print(i)
+
+# Задание 4
+    #i=0
+    #x=1
+    #y=1
+    #while x != 0 :
+    #    x = int(input())
+    #    if x > y :
+    #        i+=1
+    #    y = x
+    #print(i)
+
+# Задание 5
+    #x = int(input())
+    #i = 1
+    #while i**2 < x :
+    #    if (i+1)**2 > x :
+    #        print(i**2)
+    #        break
+    #    print(i**2)
+    #    i+=1
+
+# Задание 6
+
