175d71fbf67c00477f5403e8796966089d09f3dc
Sirotenko.EA <sirotenko.ea@esphere.ru>
2019-11-01 15:13:17 +0300

    done 1-5 from 5 tasks

--- lesson1/1_3.py
+++ lesson1/1_3.py
@@ -1,1 +1,30 @@
+# Ветвление
+# Задание 1
+    #s = input()
+    #c = s.count(' ')
+    #print(c + 1)
+
+# Задание 2
+    #s = input()
+    #t = s.index(' ')
+    #print(s[t+1:], s[:t])
+
+# Задание 3
+    #inStr = 'kng u' #input()
+    #if inStr.count('f') == 1 :
+    #    print(inStr.find('f'))
+    #elif inStr.count('f') == 0 :
+    #    print(inStr.find('f'))
+    #else :
+    #   print(inStr.find('f'), inStr.rfind('f'))
+
+# Задание 4
+    #x = input()
+    #s = x.replace('1', 'one')
+    #print(s)
+
+# Задание 5
+    #x = #input()
+    #s = x.replace('@', '')
+    #print(s)
 
